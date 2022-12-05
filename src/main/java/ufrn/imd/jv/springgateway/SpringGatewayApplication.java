f30ccef3cbe0dd28ff004cbdb1254c031b4fd984
JohnVithor <jv.venceslau.c@gmail.com>
2022-12-05 08:41:41 -0300

    adiciona formatação de arquivos

--- src/main/java/ufrn/imd/jv/springgateway/SpringGatewayApplication.java
+++ src/main/java/ufrn/imd/jv/springgateway/SpringGatewayApplication.java
@@ -7,8 +7,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class SpringGatewayApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(SpringGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGatewayApplication.class, args);
+    }
 
 }

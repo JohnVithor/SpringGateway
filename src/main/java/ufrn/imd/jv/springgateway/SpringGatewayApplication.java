1de65865a2b5068af65fe5d742638a8cae617c3e
JohnVithor <jv.venceslau.c@gmail.com>
2022-12-11 11:20:03 -0300

    altera dependencias

--- src/main/java/ufrn/imd/jv/springgateway/SpringGatewayApplication.java
+++ src/main/java/ufrn/imd/jv/springgateway/SpringGatewayApplication.java
@@ -1,8 +1,14 @@
 package ufrn.imd.jv.springgateway;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,4 +17,14 @@ public class SpringGatewayApplication {
         SpringApplication.run(SpringGatewayApplication.class, args);
     }
 
+    @LoadBalanced
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
 }

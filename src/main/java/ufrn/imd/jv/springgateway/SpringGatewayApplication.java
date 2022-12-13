b436ecf6ef14d063b9d53097f79f0ea134c12542
JohnVithor <jv.venceslau.c@gmail.com>
2022-12-13 10:09:46 -0300

    adiciona configuração de timeout

--- src/main/java/ufrn/imd/jv/springgateway/SpringGatewayApplication.java
+++ src/main/java/ufrn/imd/jv/springgateway/SpringGatewayApplication.java
@@ -2,9 +2,7 @@ package ufrn.imd.jv.springgateway;
 
 import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
